643b6e0fe461b85e2293151a3deefb929c5c01b8
BurhanuddinRaja <mails4burhan@gmail.com>
2022-05-11 19:54:51 +0530

    One Project added

--- Rock Papper Scissor/app.js
+++ Rock Papper Scissor/app.js
@@ -1,12 +1,43 @@
-const computerChoiceDisplay = document.getElementById('computer-choice')
-const userChoiceDisplay = document.getElementById('user-choice')
-const resultDisplay = document.getElementById('result')
-const possibleChoices = document.querySelectorAll('button')
-let userChoice
-let computerChoice
-let result
+const computerChoiceDisplay = document.getElementById("computer-choice");
+const userChoiceDisplay = document.getElementById("user-choice");
+const resultDisplay = document.getElementById("result");
+const possibleChoices = document.querySelectorAll("button");
 
-possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-    userChoice = e.target.innerHTML
-    userChoiceDisplay.innerHTML = userChoice
-}))
+var userChoice;
+var computerChoice;
+var result;
+
+possibleChoices.forEach((possibleChoice) => possibleChoice.addEventListener("click", (e) => {
+        userChoice = e.target.innerHTML;
+        userChoiceDisplay.innerHTML = userChoice;
+        getCompRandomNum();
+        getResult();
+    })
+);
+
+function getCompRandomNum() {
+    const randomNum = Math.floor(Math.random() * 3 + 1); // possibleChoices.length
+    if (randomNum === 1) {
+        computerChoice = "Rock";
+    }
+    if (randomNum === 2) {
+        computerChoice = "Scissors";
+    }
+    if (randomNum === 3) {
+        computerChoice = "Paper";
+    }
+    computerChoiceDisplay.innerHTML = computerChoice;
+}
+
+function getResult() {
+    if (computerChoice === userChoice) {
+        result = "its a draw!";
+    }
+    if ((computerChoice === "Rock" && userChoice === "Paper") || (computerChoice === "Paper" && userChoice === "Scissors") || (computerChoice === "Scissors" && userChoice === "Rock")) {
+        result = "you win!";
+    }
+    if ((computerChoice === "Rock" && userChoice === "Scissors") || (computerChoice === "Paper" && userChoice === "Rock") || (computerChoice === "Scissors" && userChoice === "Paper")) {
+        result = "you lost!";
+    }
+    resultDisplay.innerHTML = result;
+}
