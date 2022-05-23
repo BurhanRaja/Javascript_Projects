d1b4f81ac4dad1e0905229004e201e6298629164
BurhanuddinRaja <mails4burhan@gmail.com>
2022-05-23 12:36:16 +0530

    save button working and storing data below

--- Chrome Extension/app.js
+++ Chrome Extension/app.js
@@ -1,5 +1,22 @@
 const saveBtn = document.getElementById("save-btn")
+const inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
 
-function saveLead() {
-    console.log("Button Clicked!")
+
+// let myLeads = ["www.helloworld.com", "www.examplelead.com", "www.notyourbro.com"]
+let myLeads = []
+
+saveBtn.addEventListener("click", () => {
+    myLeads.push(inputEl.value)
+    renderLead()
+})
+
+
+function renderLead() {
+    let listitem = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        // More Human readable
+        listitem += "<li>" + myLeads[i] + "</li>"
+    }
+    ulEl.innerHTML = listitem
 }
