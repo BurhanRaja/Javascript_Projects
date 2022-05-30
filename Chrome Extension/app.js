2b496d1771d8c0a6ce30cad2b87f44a655a8e257
BurhanuddinRaja <mails4burhan@gmail.com>
2022-05-30 20:09:00 +0530

    leads to saved localStorage displayed and delete button

--- Chrome Extension/app.js
+++ Chrome Extension/app.js
@@ -1,36 +1,52 @@
+// Fetching all the ids from HTMl
 const saveBtn = document.getElementById("save-btn")
+const deleteBtn = document.getElementById("delete-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 
-// Local Storage
-localStorage.setItem("myLeads", "www.exampleleads.com")
-console.log(localStorage.getItem("myLeads"))
-localStorage.clear()
-
-
-
-// let myLeads = ["www.helloworld.com", "www.examplelead.com", "www.notyourbro.com"]
+// Initializing an empty array
 let myLeads = []
 
-saveBtn.addEventListener("click", () => {
-    myLeads.push(inputEl.value)
-    inputEl.value = ''
-    renderLead()
-})
-
-
-function renderLead() {
+// Displaying the leads on the screen
+function renderLead(array = []) {
     let listitem = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        // More Human readable
-        // listitem += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+    for (let i = 0; i < array.length; i++) {
         listitem += `
         <li>
-            <a target='_blank' href='${myLeads[i]}'>
-                ${myLeads[i]}
+            <a target='_blank' href='${array[i]}'>
+                ${array[i]}
             </a>
         </li>
         `
     }
+
+    // Rendering the list in ul element
     ulEl.innerHTML = listitem
+}
+
+// When saveBtn is clicked
+saveBtn.addEventListener("click", () => {
+
+    // Pushing the value from input element to array
+    myLeads.push(inputEl.value)
+
+    // Emptying the input bar to input new leads
+    inputEl.value = ''
+
+    // Using the localstorage to save the data (in built in chrome) and also converting the myLeads to string
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+
+    // Converting myLeads from string to array and displaying the leads
+    const storage = JSON.parse(localStorage.getItem("myLeads"))
+    if (storage) {
+        myLeads = storage
+        renderLead(myLeads)
+    }
+})
+
+// Delete button to delete the leads from localstorage, DOM and myLeads
+function deleteLeads() {
+    myLeads = []
+    localStorage.clear()
+    renderLead(myLeads)
 }
