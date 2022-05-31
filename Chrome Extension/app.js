10bb7a892e71381bc6833f314a1a3f6ffbe17024
BurhanuddinRaja <mails4burhan@gmail.com>
2022-05-31 19:45:22 +0530

    Project finished and deployed

--- Chrome Extension/app.js
+++ Chrome Extension/app.js
@@ -1,14 +1,16 @@
-// Fetching all the ids from HTMl
-const saveBtn = document.getElementById("save-btn")
-const deleteBtn = document.getElementById("delete-btn")
-const inputEl = document.getElementById("input-el")
-const ulEl = document.getElementById("ul-el")
-
 // Initializing an empty array
 let myLeads = []
 
+// Fetching all the ids from HTMl
+const saveBtn = document.getElementById("save-btn")
+const tabBtn = document.getElementById("tab-btn")
+const deleteBtn = document.getElementById("delete-btn")
+const inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
+const storage = JSON.parse(localStorage.getItem("myLeads"))
+
 // Displaying the leads on the screen
-function renderLead(array = []) {
+function render(array) {
     let listitem = ""
     for (let i = 0; i < array.length; i++) {
         listitem += `
@@ -24,29 +26,42 @@ function renderLead(array = []) {
     ulEl.innerHTML = listitem
 }
 
+// Checking if localStorage has leads
+if (storage) {
+    myLeads = storage
+    render(myLeads)
+}
+
 // When saveBtn is clicked
 saveBtn.addEventListener("click", () => {
-
+    
     // Pushing the value from input element to array
     myLeads.push(inputEl.value)
-
+    
     // Emptying the input bar to input new leads
     inputEl.value = ''
-
+    
     // Using the localstorage to save the data (in built in chrome) and also converting the myLeads to string
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
-
-    // Converting myLeads from string to array and displaying the leads
-    const storage = JSON.parse(localStorage.getItem("myLeads"))
-    if (storage) {
-        myLeads = storage
-        renderLead(myLeads)
-    }
+    
+    // Rendering the leads
+    render(myLeads)
 })
 
+// Saving Tabs url from tab button
+tabBtn.addEventListener("click", () => {
+    chrome.tabs.query({active:true, currentWindow:true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
+
+
 // Delete button to delete the leads from localstorage, DOM and myLeads
-function deleteLeads() {
+deleteBtn.addEventListener("dblclick", ()=> {
     myLeads = []
     localStorage.clear()
-    renderLead(myLeads)
-}
+    render(myLeads)
+})
+
