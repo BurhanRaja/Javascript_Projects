0d46c4ee2c5159f3a184476b2be68af4e95dd85c
BurhanuddinRaja <mails4burhan@gmail.com>
2022-09-25 20:30:47 +0530

    some UI changes added

---
+++ Password Checking/app.js
@@ -0,0 +1,12 @@
+let input = document.getElementById('password')
+// let text = document.getElementById('text')
+let weak = document.querySelector('.weak')
+
+let array = []
+
+// if (input.value.length === 0) {
+//     weak.style.display = "none"
+// }
+// else {
+//     weak.style.display = "block"
+// }
