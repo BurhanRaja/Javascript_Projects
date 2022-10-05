01028b3a68b96b6c97faba043aed2c2d9dc70ac2
BurhanuddinRaja <mails4burhan@gmail.com>
2022-10-05 20:17:07 +0530

    fetched the exchange rate data

--- Currency Converter/app.js
+++ Currency Converter/app.js
@@ -1,2 +1,33 @@
-// https://api.apilayer.com/exchangerates_data/live?base=USD&symbols=EUR,GBP
+const dropList = document.querySelectorAll(".selector select")
 
+for (let i = 0; i < dropList.length; i++) {
+    for (currency_code in country_list) {
+        let selected;
+        if (i == 0) {
+            selected = currency_code === "USD" ? 'selected' : ''
+        }
+        else if (i == 1) {
+            selected = currency_code === "INR" ? 'selected' : ''
+        }
+        let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`
+        dropList[i].insertAdjacentHTML("beforeend", optionTag)
+    }
+}
+
+const fetchExchangeRates = (base, convertedTo) => {
+    let myHeaders = new Headers();
+    myHeaders.append("apikey", secret.API_KEY);
+    
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow',
+        headers: myHeaders
+    };
+    
+    fetch("https://api.apilayer.com/exchangerates_data/convert?to=INR&from=USD&amount=1", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+}
+
+fetchExchangeRates("USD", "INR")
