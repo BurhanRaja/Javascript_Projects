a76df13ba5f37a9e6d10827cea53404e0acf655d
BurhanuddinRaja <mails4burhan@gmail.com>
2022-10-06 19:25:15 +0530

    Fetched images from URL and displayed on website

--- Currency Converter/app.js
+++ Currency Converter/app.js
@@ -1,4 +1,5 @@
 const dropList = document.querySelectorAll(".selector select")
+const imageFlag = document.querySelectorAll(".selector img")
 
 for (let i = 0; i < dropList.length; i++) {
     for (currency_code in country_list) {
@@ -14,7 +15,7 @@ for (let i = 0; i < dropList.length; i++) {
     }
 }
 
-const fetchExchangeRates = (base, convertedTo) => {
+const fetchExchangeRates = async (base, convertedTo, amount) => {
     let myHeaders = new Headers();
     myHeaders.append("apikey", secret.API_KEY);
     
@@ -24,10 +25,22 @@ const fetchExchangeRates = (base, convertedTo) => {
         headers: myHeaders
     };
     
-    fetch("https://api.apilayer.com/exchangerates_data/convert?to=INR&from=USD&amount=1", requestOptions)
+    await fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${convertedTo}&from=${base}&amount=${amount}`, requestOptions)
         .then(response => response.text())
         .then(result => console.log(result))
         .catch(error => console.log('error', error));
 }
 
-fetchExchangeRates("USD", "INR")
+const fetchFlags = (base, convertedTo) => {
+    imageFlag[0].alt = base
+    imageFlag[1].alt = convertedTo
+
+    base = base.slice(0, 2)
+    convertedTo = convertedTo.slice(0, 2)
+
+    base = base.toLowerCase()
+    convertedTo = convertedTo.toLowerCase()
+
+    imageFlag[0].src = `https://countryflagsapi.com/png/${base}`
+    imageFlag[1].src = `https://countryflagsapi.com/png/${convertedTo}`
+}
